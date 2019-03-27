199ff1da9de91b03420edcbcede67be59986a48b
任明 <octantis468@163.com>
2019-03-27 17:22:42 +0800

    update some files

--- config/index.js
+++ config/index.js
@@ -1,17 +1,19 @@
+// ./config/index.js
 module.exports = {
-  username: 'username',
-  password: 'password',
-  reponame: 'origin',
-  dir: 'Git directory path',
-  reportDir: 'Report directory path',
+  username: 'username', // Git username
+  password: 'password', // Git password
+  reponame: 'origin', // Repository name
+  dir: 'Git directory path', // /Users/viccici/github
+  reportDir: 'Report directory path', // /Users/viccici/report
   commiter: {
-    'Git name': 'Real name',
+    'Git name': 'Real name', // Git committer name matching the real name
   },
   projects: [
     {
-      name: 'Project name',
-      folder: 'Project folder',
+      name: 'Project name', // We often use chinese project name
+      folder: 'Project folder', // Git folder name that based on git path.  [ PS: weekly-report-git ]
       path: 'Git path',
+      count: true, // whether to count
     },
   ],
 };
